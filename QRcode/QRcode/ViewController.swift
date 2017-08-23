07e4ff2c91ef5514527750bd323f6a41be03c6c5
Crazy_Lin <372625840@qq.com>
2017-08-23 10:26:28 +0800

    创建项目

---
+++ QRcode/QRcode/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  QRcode
+//
+//  Created by 呆仔～林枫 on 2017/8/23.
+//  Copyright © 2017年 Lin_Crazy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
